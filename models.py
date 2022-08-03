e1c7d1afd84936186164d264c2b99ba23856ebff
aldwinp35 <aldwinp34@gmail.com>
2022-08-03 11:21:12 -0400

    create table added

--- models.py
+++ models.py
@@ -8,10 +8,10 @@ class User(db.Model):
     id: int
     username: str
     access_token: str
-        
+
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    access_token = db.Column(db.Text, unique=True, nullable=False)   
+    access_token = db.Column(db.Text, unique=True, nullable=False)
     posts = db.relationship('Post', backref='user', lazy=True)
 
     # def __repr__(self):
@@ -37,5 +37,5 @@ class Post(db.Model):
     #     return f'<Post: {self.id}>'
 
 
-# db.create_all()
-# db.session.commit()
+db.create_all()
+db.session.commit()
