ae2a4a899b41b2a500b4e5e9c67b95f18aa7b1fc
aldwinp35 <aldwinp34@gmail.com>
2022-08-14 16:09:07 -0400

    last touches

--- static/js/post-add-page.js
+++ static/js/post-add-page.js
@@ -23,7 +23,7 @@ const options = {
     content: 'Date to publish on instagram. No longer than 50 days',
     placement: 'top',
 }
-// const popover = new bootstrap.Popover(inputDate, options)
+const popover = new bootstrap.Popover(inputDate, options)
 
 // Support for mobile browser: https://stackoverflow.com/questions/20321202/not-showing-placeholder-for-input-type-date-field
 // Make input readonly when input:type=text, not show keyboard on ios
@@ -34,13 +34,14 @@ inputDate.style.backgroundColor = '#fff';
 inputDate.addEventListener('focus', () => {
     inputDate.type = 'datetime-local';
     inputDate.removeAttribute('readonly');
+    inputDate.click();
 });
 
+// If input is empty, change it to text and readonly
 inputDate.addEventListener('blur', () => {
     // Hide popover
-    // popover.hide();
+    popover.hide();
 
-    // If input is empty, change it to text and readonly
     if (inputDate.value == '')
     {
         inputDate.type = 'text';
